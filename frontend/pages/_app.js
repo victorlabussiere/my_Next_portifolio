7db96bd1509c5b85b0ddf78fcbfc84fc516d8487
Victor Labussiere <victor.labussiere95@gmail.com>
2023-02-09 15:22:49 -0300

    fix: fixed document head

--- frontend/pages/_app.js
+++ frontend/pages/_app.js
@@ -5,17 +5,9 @@ import Layout from '../components/layout/Layout'
 export default function App({ Component, pageProps }) {
   return (
     <>
-
       <Head>
-        <link href="https://fonts.googleapis.com/icon?family=Material+Icons" rel="stylesheet" />
-        <link rel="stylesheet" href="https://cdn.jsdelivr.net/gh/devicons/devicon@v2.15.1/devicon.min.css" />
-        <meta charSet='utf-8' />
-        <meta name='author' content='Victor Labussiere' />
-        <meta name='description' content='Web Application developed with Next js by Victor Labussiere' />
-        <meta name='keywords' content='React Javascript Frontend Junior UXDesigner NextJS NodeJS MirageJS' />
-        <title>Dev. Victor Labussiere</title>
+        <title>Victor Labussiere | Frontend Dev</title>
       </Head>
-
       <Layout>
         <Component {...pageProps} />
       </Layout>
--- frontend/pages/_document.js
+++ frontend/pages/_document.js
@@ -3,7 +3,14 @@ import { Html, Head, Main, NextScript } from 'next/document'
 export default function Document() {
   return (
     <Html lang="en">
-      <Head />
+      <Head>
+        <link href="https://fonts.googleapis.com/icon?family=Material+Icons" rel="stylesheet" />
+        <link rel="stylesheet" href="https://cdn.jsdelivr.net/gh/devicons/devicon@v2.15.1/devicon.min.css" />
+        <meta charSet='utf-8' />
+        <meta name='author' content='Victor Labussiere' />
+        <meta name='description' content='Web Application developed with Next js by Victor Labussiere' />
+        <meta name='keywords' content='React Javascript Frontend Junior UXDesigner NextJS NodeJS MirageJS' />
+      </Head>
       <body>
         <Main />
         <NextScript />
